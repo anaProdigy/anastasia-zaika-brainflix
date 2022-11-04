58bd8434655aa6c49b15ede805268789d9350beb
Anastasia Zaika <z.anastasia11@gmail.com>
2022-11-03 23:27:30 -0700

    style phone page

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Button from "../../assets/images/Icons/add_comment.svg";
 import Avatar from "../../assets/images/Mohan-muruge.jpg";
 
-import SimpleDateTime from "react-simple-timestamp-to-date";
+import SimpleDateTime from 'react-simple-timestamp-to-date';
 
 
 function Comments({comments}) {
   return (
     <section className="comments">
       <h1 className="comments__amount">3 Comments</h1>
-      <h2 className="comments__title">join the conversation</h2>
+      <h2 className="comments__title">JOIN THE CONVERSATION</h2>
 
       <div className="comments__holder">
         <img className="comments__avatar" src={Avatar} alt="user image" />
@@ -28,11 +28,17 @@ function Comments({comments}) {
 
       {comments.map((comment) => (
         <section className="comments__container" key={comment.name}>
-          <h3 className="comments__name">{comment.name}</h3>
-          <SimpleDateTime dateFormat="DMY" dateSeparator="/" showTime="0"
-            className="comments__timestamp">
-            {comment.timestamp}
-          </SimpleDateTime>
+          <div className="comments__name-time">
+            <h3 className="comments__name">{comment.name}</h3>
+            <SimpleDateTime
+              dateFormat="DMY"
+              dateSeparator="/"
+              showTime="0"
+              className="comments__timestamp"
+            >
+              {comment.timestamp}
+            </SimpleDateTime>
+          </div>
           <p className="comments__comment">{comment.comment}</p>
         </section>
       ))}
--- src/components/Description/Description.jsx
+++ src/components/Description/Description.jsx
@@ -1,5 +1,7 @@
 import "./description.scss";
-
+import SimpleDateTime from "react-simple-timestamp-to-date";
+import Views from "../../assets/images/Icons/views.svg";
+import Likes from "../../assets/images/Icons/likes.svg";
 
 function Description({ videoDetails }) {
   const {
@@ -16,18 +18,37 @@ function Description({ videoDetails }) {
     <section className="description">
       <section className="description-one">
         <div className="description-one__poster">
-          <video className="description-one__poster-img" poster={image}></video>
+          <video
+            control="controls"
+            src="true"
+            className="description-one__poster-img"
+            controls
+            poster={image}
+          ></video>
         </div>
         <h1 className="description-one__title">{title}</h1>
         <div className="description-one__num-text">
           <div className="description-one__num">
-            <div>
-              <p>{channel}</p>
-              <div>{timestamp}</div>
+            <div className="description-one__num-date">
+              <div className="description-one__num-channel">By {channel}</div>
+              <SimpleDateTime
+                className="description__timestamp"
+                dateFormat="DMY"
+                dateSeparator="/"
+                showTime="0"
+              >
+                {timestamp}
+              </SimpleDateTime>
             </div>
-            <div>
-              <div>{views}</div>
-              <div>{likes}</div>
+            <div className="description-one__num-likes">
+              <div className="description__views-text">
+                <img className="description__views" src={Views} />
+                {views}
+              </div>
+              <div className="description__likes-text">
+                <img className="description__likes" src={Likes} />
+                {likes}
+              </div>
             </div>
           </div>
           <p className="description-one__text">{description}</p>
