9c8a58bbf5d5b1efde7a5dbf3fcc0c2a6ce0e24c
Anastasia Zaika <z.anastasia11@gmail.com>
2022-11-21 22:27:43 -0800

    add comments

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -14,7 +14,7 @@ function Comments({comments, videoId}) {
     e.preventDefault();
 
     axios.post(
-`http://localhost:8080/videos/${videoId}/comments`
+`http://localhost:8080/videos/${videoId}/comments`, {comment:e.target.comment.value, name:e.target.name.value}
     )
 console.log("clickComment")
   }
@@ -23,22 +23,27 @@ console.log("clickComment")
   return (
     <>
       <section className="comments">
-        <h1 className="comments__amount">
-          {comments?.length} Comments
-        </h1>
+        <h1 className="comments__amount">{comments?.length} Comments</h1>
         <h2 className="comments__title">JOIN THE CONVERSATION</h2>
 
         <div className="comments__holder">
           <img className="comments__avatar" src={Avatar} alt="user avatar" />
 
-          <form className="comments__form"  onSubmit ={(event) => clickComment(event)}>
+          <form
+            className="comments__form"
+            onSubmit={(event) => clickComment(event)}
+          >
             <textarea
+              name="name"
+              className="comments__form-input"
+              placeholder="Add a new name"
+            ></textarea>
+            <textarea
+              name="comment"
               className="comments__form-input"
               placeholder="Add a new comment"
             ></textarea>
-            <button className="comments__form-btn">
-              COMMENT
-            </button>
+            <button className="comments__form-btn">COMMENT</button>
           </form>
         </div>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,12 +7,14 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-const api_key ="b1993973-f705-4b41-9e54-bc62a8aa7e81";
+const api_key = "b1993973-f705-4b41-9e54-bc62a8aa7e81";
 
 function Home() {
   const [videos, setVideos] = useState([]);
   const [videoDetails, setVideoDetails] = useState({});
   const { videoId } = useParams();
+
+  const id = videoId || "84e96018-4022-434e-80bf-000ce4cd12b8";
 
   useEffect(() => {
     axios
@@ -22,7 +24,7 @@ function Home() {
       })
       .catch((error) => console.log(error));
 
-    const id = videoId || "84e96018-4022-434e-80bf-000ce4cd12b8";
+    //const id = videoId || "84e96018-4022-434e-80bf-000ce4cd12b8";
 
     axios.get(`http://localhost:8080/videos/${id}`).then((response) => {
       //console.log("Data", response); id= videoId
@@ -41,7 +43,7 @@ function Home() {
           <Comments
             comments={videoDetails?.comments && videoDetails?.comments}
             videoDetails={videoDetails && videoDetails}
-            videoId = {videoId}
+            videoId={id}
           />
         </section>
         <VideoInfo
